25fb196163df6aabea10f70bf97da03dd881c834
Shoyuj <shoyujsharma@gmail.com>
2026-07-16 11:40:57 +0530

    [965] [Time Beats: 100.00%] [Memory Beats: 77.39%] - LeetPush

---
+++ 965-Univalued-Binary-Tree.java
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean check(TreeNode node,int val)
+    {
+        if(node==null )
+        {
+            return true;
+        }
+        if(node.val!=val)
+        {
+            return false;
+        }
+        return check(node.left,val) && check(node.right,val);
+    }
+    public boolean isUnivalTree(TreeNode root) {
+    int val=root.val;
+    return check(root,val); 
+    }
+}
