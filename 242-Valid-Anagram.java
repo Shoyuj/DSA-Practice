36c11b52b8b2b8359f9de314b8fb115111d4f3a4
Shoyuj <shoyujsharma@gmail.com>
2026-09-01 10:58:19 +0530

    [242] [Time Beats: 94.08%] [Memory Beats: 66.50%] - LeetPush

--- 242-Valid-Anagram.java
+++ 242-Valid-Anagram.java
@@ -1,30 +1,16 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-
-        if (s.length() != t.length()){
-            return false;
+        if(s.length() != t.length()) return false;
+        int[] arr= new int[26];
+        for(int i=0;i<s.length();i++){
+            arr[s.charAt(i)-'a']++;
         }
-
-        int[] dataHolder = new int [26];
-        int ordinal = (int) 'a';
-
-
-        for (char i : s.toCharArray()){
-            dataHolder[(int)i- ordinal] += 1;
-
+        for(int j=0;j<t.length();j++){
+            arr[t.charAt(j)-'a']--;
         }
-
-        for (char j : t.toCharArray()){
-            dataHolder[(int)j- ordinal] -= 1;
-
+        for(int i=0;i<arr.length;i++){
+            if(arr[i] !=0)return false;
         }
-        for (int l : dataHolder){
-            if(l !=0 ){
-                return false;
-            }
-
-        }
-        return true;
-        
+    return true;
     }
 }
